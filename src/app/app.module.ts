201aab2408ee2859c393328f5114cf1a3b3ef983
sebgos <gos.sebastian@gmail.com>
2020-05-20 21:06:47 +0200

    feat: add PDFMake CV example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostingsService } from './services/postings.service';
+import { PostingsService } from './services/postings/postings.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+  ],
   providers: [PostingsService],
   bootstrap: [AppComponent],
 })
