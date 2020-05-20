685419bd765fac55b6f876ce3963da3dfc6e1ba1
sebgos <gos.sebastian@gmail.com>
2020-05-20 19:55:16 +0200

    feat: integrate jspdf with postings service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import { PostingsService } from './services/postings/postings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular9-print-to-template';
+export class AppComponent implements OnInit {
+  @ViewChild('htmlData') htmlData: ElementRef;
+  postings;
+  currentDate;
+
+  constructor(private postingsService: PostingsService) {}
+  ngOnInit() {
+    this.currentDate = new Date();
+    this.postings = this.postingsService.getPostings();
+  }
+  public openPDF(): void {
+    const DATA = this.htmlData.nativeElement;
+    const doc = new jsPDF('p', 'pt', 'a4');
+    doc.fromHTML(DATA.innerHTML, 15, 15);
+    doc.output('dataurlnewwindow');
+  }
+
+  public downloadPDF(): void {
+    const DATA = this.htmlData.nativeElement;
+    const doc = new jsPDF('p', 'pt', 'a4');
+
+    const handleElement = {
+      '#editor'(element, renderer) {
+        return true;
+      },
+    };
+    doc.fromHTML(DATA.innerHTML, 15, 15, {
+      width: 200,
+      elementHandlers: handleElement,
+    });
+
+    doc.save('angular9-print-to-template-jspdf.pdf');
+  }
 }
